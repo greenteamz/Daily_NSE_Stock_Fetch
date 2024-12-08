b03f1101956b6192343255b0bca74a54cfb6d428
Aravinthkumar Jayaprakasam <aravinthkumarj1@gmail.com>
2024-12-08 10:05:56 +0530

    removed schema

--- scripts/Daily_NSE_bigQuery.py
+++ scripts/Daily_NSE_bigQuery.py
@@ -340,8 +340,7 @@ def load_data_to_bigquery():
             job_config = bigquery.LoadJobConfig(
                 source_format=bigquery.SourceFormat.CSV,
                 skip_leading_rows=1,  # Skip header row
-                write_disposition="WRITE_APPEND",  # Append data
-                schema=schema,
+                write_disposition="WRITE_APPEND",  # Append data schema=schema
                 autodetect=False,
             )
             load_job = bq_client.load_table_from_file(
